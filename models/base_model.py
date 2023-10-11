7c148e5dda745978c83a724eaf6e52ee048fe679
mefdev <mohamedmeftah2018@gmail.com>
2023-10-11 22:45:15 +0100

    Refactor base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """Define all common attributes/methods for other classes"""
@@ -12,7 +12,9 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
 
+       
         if kwargs is not {} or None:
             for key, value in kwargs.items():
                 if key in ("created_at, updated_at"):
@@ -22,7 +24,7 @@ class BaseModel:
                         self.__dict__[key] = str(value)
                     else:   
                         self.__dict__[key] = value
-
+                
         
         
 
@@ -33,18 +35,21 @@ class BaseModel:
     def save(self):
         """Update the instance attribute"""
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary representation of an instance"""
-        created_at_formatted = {"created_at": self.created_at.isoformat()}
-        updated_at_formatted = {"updated_at": self.updated_at.isoformat()}
+        created_at_formatted = {"created_at": self.created_at.isoformat() if isinstance(self.created_at, datetime) else self.created_at}
+        updated_at_formatted = {"updated_at": self.updated_at.isoformat() if isinstance(self.updated_at, datetime) else self.updated_at}
         add_class = {"__class__": self.__class__.__name__}
-
+        
         arr = [created_at_formatted, updated_at_formatted, add_class]
         for item in arr:
             self.__dict__.update(item)
 
         return self.__dict__
+
+
 
 
 
