bb919fd545243c6df7079edf133000dd10e1f25b
mefdev <mohamedmeftah2018@gmail.com>
2023-10-12 01:23:44 +0100

    Work on update

--- console.py
+++ console.py
@@ -24,7 +24,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         elif list[0] != "BaseModel":
             print("** class doesn't exist **")
-        elif list[1] == "":
+        elif len(list) < 2:
             print("** instance id missing **")
         else:
             all_objs = storage.all()
@@ -41,16 +41,99 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, args):
             list = args.split(" ")
-            if (len(list) == 0):
+            if (list[0] == ''):
                 print("** class name missing **")
+                return
             elif list[0] != "BaseModel":
                 print("** class doesn't exist **")
-            elif list[1] == "":
+                return
+            elif len(list) < 2:
                 print("** instance id missing **")
-            else:
-                my_model = BaseModel()
-                my_model.save()
-                print(my_model.id)
+                return
+            
+            key = list[0] + '.' + list[1]
+            if key not in storage.all():
+                 print("** no instance found **")
+                 return
+            ## Delete the instance
+            del storage.all()[key]
+            storage.save()
+
+        
+    def do_all(self, args):
+        #
+        list = args.split(" ")
+        test_var = 0
+        
+        if (list[0] == '' and len(list) == 1):
+            test_var = 1
+            list_of_dics = [] # create empty dictionary
+            all_objs = storage.all() # get the storage
+            for obj_id in all_objs.keys(): # loop through the storage
+                list_of_dics.append(str(all_objs[obj_id]))
+            print(list_of_dics)
+        if len(list) > 0 and list[0] == "BaseModel":
+            list_of_dics = [] # create empty dictionary
+            all_objs = storage.all() # get the storage
+            for obj_id in all_objs.keys(): # loop through the storage
+                class_name, class_id = obj_id.split(".") # split the obj [basemodel].id
+                if (class_name == list[0]):
+                    list_of_dics.append(str(all_objs[obj_id]))
+            print(list_of_dics)
+        elif (len(list) > 0 and list[0] != "BaseModel" and test_var == 0): 
+                print("** class doesn't exist **")
+
+
+
+    def do_update(self, args):
+        list = args.split(" ")
+        if (list[0] == ''):
+            print("** class name missing **")
+            return
+        if list[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(list) < 2:
+            print("** instance id missing **")
+            return
+            
+        if len(list) < 3:
+            print("** attribute name missing **")
+            return
+        if len(list) < 4:
+            print("** value missing **")
+            return
+        
+        key = list[0] + '.' + list[1]
+        if key not in storage.all():
+                print("** no instance found **")
+                return
+        if (len(list) == 4):
+            all_objs = storage.all() # get the storage
+            for obj_id in all_objs.keys(): # loop through the storage
+                class_name, class_id = obj_id.split(".") # split the obj [basemodel].id
+                if (class_name == "BaseModel" and list[3] == "__class__"):
+                    storage.all()["__class__"] = list[4]
+                    print(storage.all()["__class__"])
+
+
+             
+    
+    def help_update(self, args):
+        print("update <class name> <id> <attribute name> '<attribute value>'")
+
+            
+        
+        
+    
+        
+    
+           
+              
+
+
+
+                
        
     
     def do_quit(self, arg):
