a3d9859656856049771042a0eb215c7ea0e9df13
mefdev <mohamedmeftah2018@gmail.com>
2023-10-14 23:09:46 +0100

    Implement <classname>.id feature

--- console.py
+++ console.py
@@ -187,10 +187,10 @@ class HBNBCommand(cmd.Cmd):
         """Quit the CLI"""
         return True
 
-    def emptyline(self) :
+    def emptyline(self):
         """Does Nothing"""
         pass
-    
+
     def help_quit(self):
         """Quit the CLI"""
         print("Quit command to exit the program")
